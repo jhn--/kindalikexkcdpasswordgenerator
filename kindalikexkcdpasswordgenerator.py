a4ce1c5f122cd8c69652c924e9329369327066fe
John <john.hoe@gmail.com>
2021-03-20 06:28:01 +0800

    popular _ALPHABET based on the unique letters in word provided by user. ought to save memory and time.
    haven't test, commit and push first #badpractice

--- kindalikexkcdpasswordgenerator.py
+++ kindalikexkcdpasswordgenerator.py
@@ -3,39 +3,14 @@
 import sys
 from random import randrange
 
-_ALPHABET = {
-    "a":[],
-    "b":[],
-    "c":[],
-    "d":[],
-    "e":[],
-    "f":[],
-    "g":[],
-    "h":[],
-    "i":[],
-    "j":[],
-    "k":[],
-    "l":[],
-    "m":[],
-    "n":[],
-    "o":[],
-    "p":[],
-    "q":[],
-    "r":[],
-    "s":[],
-    "t":[],
-    "u":[],
-    "v":[],
-    "w":[],
-    "x":[],
-    "y":[],
-    "z":[]
-}
+_ALPHABET = {}
 
 _PASSWORD = []
 
+
 class Error(Exception):
     pass
+
 
 class YouNeedAWordError(Error):
     def __init__(self, message):
@@ -44,28 +19,31 @@ class YouNeedAWordError(Error):
     def __str__(self):
         return f'{self.message}'
 
-def get_words():
+
+def get_words(input_set):
     with open('./words_alpha.txt', 'r') as fp:
         words = fp.readlines()
-        for i in _ALPHABET.keys():
-            _ALPHABET[i] = [x.strip('\n') for x in words if x[0] == i and len(x) <= 6]
+
+        for i in input_set:
+            _ALPHABET[i] = [x.strip('\n')
+                            for x in words if x[0] == i and len(x) <= 6]
+
 
 def main(w):
-    # print(f'{w}')
+    wordd_set = set(wordd)
 
-    get_words()
+    get_words(wordd_set)
 
-    # for i in _ALPHABET.keys():
-    #     print(f'{len(_ALPHABET[i])}')
-    
     for i in w:
         _PASSWORD.append(_ALPHABET[i][randrange(len(_ALPHABET[i]))])
-    
+
     print(f'Password generated: {(" ").join(_PASSWORD)}')
+
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
         wordd = sys.argv[1]
         main(wordd)
     else:
-        raise YouNeedAWordError("BRO, ENTER A WORD! Can't do what I need to do without a word broooooo!")
+        raise YouNeedAWordError(
+            "BRO, ENTER A WORD! Can't do what I need to do without a word broooooo!")
